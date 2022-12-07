aa4d2431703b6a03cd922adc8a1582a1b046a0bc
Youssef <joekamel08@gmail.com>
2022-12-07 21:38:09 +0200

    My Class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Hahahahahaha");
+        System.out.println("new projdfsdfect");
+        System.out.println("new projdfsdfect");
         System.out.println("new projdfsdfect");
     }
 
